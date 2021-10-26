b4a1d58e3aa624d69c7979a2a1f1ed650add3376
Gelila12 <75943633+Gelila12@users.noreply.github.com>
2021-10-26 00:06:18 -0500

    maven-surefire-report-plugin remove

--- src/test/java/testCases/LoginPageTest.java
+++ src/test/java/testCases/LoginPageTest.java
@@ -26,9 +26,9 @@ public class LoginPageTest extends TestBase {
 	@Test(priority = 1)
 	public void LoginTest() throws ClassNotFoundException, SQLException, InterruptedException, IOException {
 		loginpageObj.insertUserName("Demo@techfios.com");
-		Thread.sleep(6000);
+		Thread.sleep(3000);
 		loginpageObj.insertPassword("abc123");
-		Thread.sleep(6000);
+		Thread.sleep(3000);
 		loginpageObj.clickOnSigninButton();
 		takeScreenshotAtEndOfTest(driver);
 	}
@@ -36,9 +36,9 @@ public class LoginPageTest extends TestBase {
 	@Test(priority = 2)
 	public void LoginPageTitleTest() throws ClassNotFoundException, SQLException, InterruptedException {
 		loginpageObj.insertUserName("Demo@techfios.com");
-		Thread.sleep(6000);
+		Thread.sleep(3000);
 		loginpageObj.insertPassword("abc123");
-		Thread.sleep(6000);
+		Thread.sleep(3000);
 		loginpageObj.clickOnSigninButton();
 
 		String expectedTitle = "Dashboard- iBilling";
