72d76b0c4ad9dc79ffc3c93addaecb919529be2b
Gelila12 <75943633+Gelila12@users.noreply.github.com>
2021-10-26 23:05:34 -0500

    maven-surefire-report-plugin remove

--- src/test/java/testCases/LoginPageTest.java
+++ src/test/java/testCases/LoginPageTest.java
@@ -1,14 +1,10 @@
 package testCases;
 
-import java.io.IOException;
-import java.sql.SQLException;
-
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import base.TestBase;
 import page.LoginPage;
 public class LoginPageTest extends TestBase {
@@ -23,21 +19,21 @@ public class LoginPageTest extends TestBase {
 	}
 
 	@Test(priority = 1)
-	public void LoginTest() throws  InterruptedException, ClassNotFoundException, SQLException, IOException  {
+	public void LoginTest() throws Throwable   {
 		loginpageObj.insertUserName("Demo@techfios.com");
-		Thread.sleep(4000);
+		Thread.sleep(2000);
 		loginpageObj.insertPassword("abc123");
-		Thread.sleep(4000);
+		Thread.sleep(2000);
 		loginpageObj.clickOnSigninButton();
 		takeScreenshotAtEndOfTest(driver);
 	}
 
 	@Test(priority = 2)
-	public void LoginPageTitleTest() throws    InterruptedException, ClassNotFoundException, SQLException, IOException   {
+	public void LoginPageTitleTest() throws  Throwable    {
 		loginpageObj.insertUserName("Demo@techfios.com");
-		Thread.sleep(4000);
+		Thread.sleep(2000);
 		loginpageObj.insertPassword("abc123");
-		Thread.sleep(4000);
+		Thread.sleep(2000);
 		loginpageObj.clickOnSigninButton();
 
 		String expectedTitle = "Dashboard- iBilling";
